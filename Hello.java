2e04138d2978faa7d720e2fccf85a3546565cf7b
Mr Sagar <154483781+sagarkuranagi@users.noreply.github.com>
2026-03-04 16:27:52 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("args:"+ Arrays.toString(args));
+        System.out.println(args[0]);
+        System.out.println("Hello Word!!");
+    }
+}
